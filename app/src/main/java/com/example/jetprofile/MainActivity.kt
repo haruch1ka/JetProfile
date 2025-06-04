a7a643897ff757f1b0398c0e97383ae64ff41141
d-ito <sican1868@gmail.com>
2025-06-04 22:32:03 +0900

    feat: Update MainActivity layout with image styling and padding

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,12 +33,19 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(20.dp),
+
+                        ) {
                         Image(
                             painter = painterResource(id = R.drawable.icon),
                             contentDescription = "プロフィール",
+                            modifier = Modifier
+                                .size(150.dp)
+                                .clip(RoundedCornerShape(10.dp))
+
                         )
+                        Spacer(modifier = Modifier.height(20.dp))
                         Text(
                             text = "伊藤　大樹",
                             color = Color.Gray,
